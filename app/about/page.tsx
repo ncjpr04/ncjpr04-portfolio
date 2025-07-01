77edd3d0220a90efa39188f9d5023822d8e045ba
Nitin Choudhary <nitinchoudhary22112004@gmail.com>
2025-07-02 00:13:01 +0530

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -84,7 +84,8 @@ export default async function About() {
                       View Résumé <BiLinkExternal className="text-base" />
                     </RefLink>
                     <a
-                      href={`${profile?.resumeURL}?dl=${profile?.fullName}-resume.pdf`}
+                      // href={`${profile?.resumeURL}?dl=${profile?.fullName}-resume.pdf`}
+                      href="https://drive.google.com/file/d/1x9d8Eb4p33i8zB5HogSW_oODDlmmz4pE/view?usp=sharing"
                       className="flex items-center justify-center text-center dark:text-primary-color text-secondary-color hover:underline basis-[10%] dark:bg-primary-bg bg-zinc-100 border border-transparent dark:hover:border-zinc-700 hover:border-zinc-200 rounded-md py-3 text-lg"
                       title="Download Resume"
                     >
